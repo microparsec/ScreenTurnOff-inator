64a86ce64908c9a30ea60eb4c59d13c2e63e902e
Casper Bezemer <cspr@bzmr.nl>
2018-07-07 13:18:17 +0200

    Initial Version

---
+++ ScreenTurnOff-inator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenTurnOff_inator
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+        static void Main(string[] args)
+        {
+            SendMessage(-1, 0x0112, 0xF170, 2);
+        }
+    }
+}
